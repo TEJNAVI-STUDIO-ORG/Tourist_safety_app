d84d2b2b0e165cbe3ac74e2cbfc60364dbc10b62
Adityasonar29 <adityavispute29@gmail.com>
2026-06-21 14:20:15 +0530

    updated the version

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -416,7 +416,7 @@ class _SettingsScreenState extends State<SettingsScreen>
 
                     title: const Text("TouriSafe"),
 
-                    subtitle: const Text("Version 2.40.23"),
+                    subtitle: const Text("Version 2.40.30"),
                   ),
 
                   const Divider(height: 1),
